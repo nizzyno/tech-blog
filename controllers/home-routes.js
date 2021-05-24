9e716c182e988a6f0e34a28d3e2d8645d26eb0bb
Noah <mcbeasty11.nh@icloud.com>
2021-05-24 15:12:27 -0500

    fixed local path in main.handlbars for stylesheet

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,7 @@
+const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
-const router = require('express').Router();
+
 router.get('/', (req, res) => {
   Post.findAll({
     attributes: ['id', 'title', 'content', 'created_at'],
