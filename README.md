1a46aa0de297037fd5c59a8a6c4155746fb7438c
Noah <mcbeasty11.nh@icloud.com>
2021-05-24 13:27:39 -0500

    added heroku link to readme.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Tech Blog
+# Tech Blog - https://immense-journey-64895.herokuapp.com
 
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const path = require('path');
-const helpers = require('./utils/helpers');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
+const routes = require('./controllers');
+const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3033;
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 const sess = {
-  secret: 'sharedsecret',
+  secret: 'Super secret secret',
   // 10 mins = 10*60*60 = 36000
   cookie: { maxAge: 36000 },
   resave: false,
@@ -20,13 +19,14 @@ const sess = {
     db: sequelize,
   }),
 };
+
 app.use(session(sess));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 // turn on connection to db and server
